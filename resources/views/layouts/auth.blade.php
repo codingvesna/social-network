b5f53050c2532df29e22a6ad8090f5e88792340a
codingvesna <codingvesna1@gmail.com>
2021-04-17 19:17:51 +0200

    register view - design phase

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Admin - @yield('title')</title>
+    <title>lemonade - @yield('title')</title>
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Montserrat" rel="stylesheet">
     <link href="{{ asset('fontisto/css/fontisto/fontisto.min.css') }}" rel="stylesheet">
