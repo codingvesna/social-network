0207b515f4f46f1efb10724673e749fe1d27eb6a
codingvesna <codingvesna1@gmail.com>
2021-04-30 09:45:12 +0200

    responsive nav & redesign

--- resources/views/partials/header/navigation.blade.php
+++ resources/views/partials/header/navigation.blade.php
@@ -1,37 +1,43 @@
-<ul class="w-full hidden lg:flex flex-row justify-end items-center">
-    <li><h3 class="text-p text-base mr-5 align-middle">{{ auth()->user()->name }}</h3></li>
-    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded mr-3 hover:border-queen-blue hover:bg-queen-blue hover:shadow" style="width: 40px; height: 40px;"
+<ul class="w-full flex flex-row justify-end items-center">
+{{--    <li><h3 class="text-p text-base mr-5 align-middle">{{ auth()->user()->name }}</h3></li>--}}
+    <li class="navicon nav-icon" style="width: 40px; height: 40px;"
         title="Notifikacije">
         <a href="#">
             <span class="p-3 align-middle"><i class="fi fi-bell text-lg text-icon"></i></span>
         </a>
     </li>
 
-    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded mr-3 hover:border-fiery-red hover:bg-fiery-red hover:shadow" style="width: 40px; height: 40px;"
+    <li class="navicon nav-icon" style="width: 40px; height: 40px;"
         title="Favoriti">
         <a href="#">
             <span class="p-3 align-middle"><i class="fi fi-heart-alt text-lg text-icon"></i></span>
         </a>
     </li>
 
-    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded mr-3 hover:border-queen-blue hover:bg-queen-blue hover:shadow" style="width: 40px; height: 40px;"
+    <li class="navicon nav-icon" style="width: 40px; height: 40px;"
         title="Moje grupe">
         <a href="#">
             <span class="p-3 align-middle"><i class="fi fi-persons text-lg text-icon"></i></span>
         </a>
     </li>
 
-    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded mr-3 hover:border-queen-blue hover:bg-queen-blue hover:shadow" style="width: 40px; height: 40px;"
+    <li class="navicon nav-icon" style="width: 40px; height: 40px;"
         title="Moj nalog">
         <a href="#">
             <span class="p-3 align-middle"><i class="fi fi-person text-lg text-icon"></i></span>
         </a>
     </li>
 
-    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded hover:border-queen-blue hover:bg-queen-blue hover:shadow" style="width: 40px; height: 40px;"
-        title="Izloguj se">
-        <a href="{{ route('logout') }}">
-            <span class="p-3 align-middle"><i class="fi fi-power text-lg text-icon"></i></span>
+{{--    <li class="nav-icon flex justify-center items-center border-solid border-1 border-transparent rounded hover:border-queen-blue hover:bg-queen-blue hover:shadow" style="width: 40px; height: 40px;"--}}
+{{--        title="Izloguj se">--}}
+{{--        <a href="{{ route('logout') }}">--}}
+{{--            <span class="p-3 align-middle"><i class="fi fi-power text-lg text-icon"></i></span>--}}
+{{--        </a>--}}
+{{--    </li>--}}
+
+    <li class="navicon nav-icon" >
+        <a href="#">
+            <img class="inline object-cover w-8 h-8 rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile picture"/>
         </a>
     </li>
 </ul>
