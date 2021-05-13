f173f7f0ed5da86830eb2de0b17de6e6ae144a6e
codingvesna <codingvesna1@gmail.com>
2021-05-13 12:47:08 +0200

    logo change

--- resources/views/partials/header/nav.blade.php
+++ resources/views/partials/header/nav.blade.php
@@ -2,7 +2,7 @@
     @if (auth()->user())
     <div class="w-2/12 flex justify-start">
         <a href="{{ route('dashboard') }}" title="Dashboard">
-            <img src="images/logo.jpg" alt="logo" class="object-scale-down logo"/>
+            <img src="images/logosocial.jpg" alt="logo" class="object-scale-down logo"/>
         </a>
     </div>
     <div class="hidden sm:w-4/12 sm:flex">
