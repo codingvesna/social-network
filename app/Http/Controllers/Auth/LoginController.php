d57f0891777cc0b35cbec4cc8d6f38af9647f825
codingvesna <codingvesna1@gmail.com>
2021-05-14 13:01:51 +0200

    remember me

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -16,12 +16,13 @@ class LoginController extends Controller
     }
 
     public function store(Request $request){
+        //dd($request->remember);
         $this->validate($request, [
             'email' => "required|email",
             'password' =>"required"
         ]);
 
-        if (!auth()->attempt($request->only('email', 'password'))) {
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
             return back()->with('status', 'Podaci nisu validni.');
         }
         return redirect()->route('dashboard');
