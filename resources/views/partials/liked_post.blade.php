6fdcb671969e55b6f553a02862c41e36e1b2c4e5
codingvesna <codingvesna1@gmail.com>
2021-05-17 09:43:43 +0200

    authorization - delete posts - favourites view

--- resources/views/partials/liked_post.blade.php
+++ resources/views/partials/liked_post.blade.php
@@ -11,14 +11,32 @@
                                 <img class="inline object-cover w-8 h-8 md:w-12 md:h-12 rounded-full" src="https://images.pexels.com/photos/2589653/pexels-photo-2589653.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="profile picture"/>
                             </a>
                         </div>
-                        <div class="w-10/12 flex flex-col justify-start items-start">
-                            <a href="#">
-                                <span class="font-bold text-base text-p">{{$post->user->name}} {{$post->user->last_name}} </span>
-                            </a>
-                            <span class="text-xs font-normal text-queen-blue"> {{$post->created_at->toTimeString()}}</span>
+
+                        <div class="w-10/12 flex items-center justify-between">
+                            <div class="flex flex-col justify-start items-start">
+                                <a href="#">
+                                    <span class="font-bold text-base text-p">{{$post->user->name}} {{$post->user->last_name}} </span>
+                                </a>
+                                <span class="text-xs font-normal text-queen-blue"> {{$post->created_at->toTimeString()}}</span>
+                            </div>
+                            {{--                            @if($post->ownedBy(auth()->user()))--}}
+                            @can('delete', $post)
+                                <div>
+                                    <form action="{{ route('posts.delete', $post) }}" method="post">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" title="Obrisi" class="text-sm text-queen-blue hover:text-fiery-red focus:outline-none">
+                                            <i class="fi fi-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                            @endcan
+                            {{--                            @endif--}}
                         </div>
                     </div>
                 </div>
+
+
                 <div class="w-full border-b-1 border-solid border-border px-4 py-4">
                     <p class="text-p">{{ $post->post }}</p>
                 </div>
